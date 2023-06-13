26a712f489171c6054980dcde88b3304d47082dc
irene <panisirene@gmail.com>
2023-06-12 17:25:51 -0700

    add comments

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const questions = [
     name: 'text',
     type: 'input',
     message: 'Please enter up to 3 characters to be featured on your logo.',
+    // validate if input is 3 chars
     validate: (input) => {
       if (input.length > 3) {
         return "Your input is too long! Please keep it to 3 characters or shorter!";
@@ -21,9 +22,10 @@ const questions = [
     name: 'textColor',
     type: 'input',
     message: 'Please enter a color keyword for your TEXT.',
+    // validates if color keyword exists
     validate: (input) => {
       if (!colors.includes(input.toLowerCase())) {
-        return 'Not a valid color! Check out https://developer.mozilla.org/en-US/docs/Web/CSS/named-color for valid color keywords -- currently only the 16 standard colors are accepted.';
+        return 'Not a valid color! Check out https://developer.mozilla.org/en-US/docs/Web/CSS/named-color for valid color keywords.';
       }
       return true; 
     }
@@ -42,25 +44,29 @@ const questions = [
     name: 'shapeColor',
     type: 'input',
     message: 'Please enter a color keyword for your SHAPE.',
+    // validates if color keyword exists
     validate: (input) => {
       if (!colors.includes(input.toLowerCase())) {
-        return 'Not a valid color! Check out https://developer.mozilla.org/en-US/docs/Web/CSS/named-color for valid color keywords -- currently only the 16 standard colors are accepted.';
+        return 'Not a valid color! Check out https://developer.mozilla.org/en-US/docs/Web/CSS/named-color for valid color keywords.';
       }
       return true; 
     }
   },
 ];
 
+// writes svg content into file
 function writeToFile(fileName, shape) {
   fs.writeFile(fileName, shape.render(), (err) => 
     err ? console.log(err) : console.log("Generated logo.svg in examples/ folder")
   );
 }
 
+// initialize to start prompts
 function init() {
   inquirer
     .prompt(questions)
     .then((data) => {
+      // switch statement to handle different shapes
       switch(data.shape) {
         case "Triangle":
           const newTriangle = new shapes.Triangle(data.text, data.textColor, data.shapeColor);
